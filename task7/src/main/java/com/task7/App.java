a9856c40244b934e844e93fd32cbe714164831ea
Aleksandr Ivakin <pwian@mail.ru>
2021-02-22 10:10:22 +0300

    add task7

---
+++ task7/src/main/java/com/task7/App.java
@@ -0,0 +1,13 @@
+package com.task7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
